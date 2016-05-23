85da4d3c93e5ae9234b5b83c97542b338122c2b6
Miles Dahmann <mdahmann@gmail.com>
2016-05-23 14:35:05 -0500

    Fonts added, initial structure, custom JS

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+
+// Tugg
+// =================
+//<![CDATA[
+  TUGG_WG_INITIAL_VIEW = 'map'; 								// Set to ‘map’ or ‘list’ as your initial view
+  TUGG_WG_EL  = '#tg-title-widget-example';     // dom element to insert Tugg widget content
+  API_KEY = 'VHGi4IIqSooo9qk';
+  SHOW_HOST_FILM_EVENT_BTN = false;							// show/hide host film event button in the widget
+  SHOW_SUBSCRIBE_FOR_UPDATES_BTN = false;				// show/hide subscribe for updates button in the widget
+  CAMPAIGN_SLUG = 'at-the-fork';
+
+//]]>
+// Smooth Scrolling for Anchors Links
+// =================
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 100
+                }, 750);
+                return false;
+            }
+        }
+    });
+});
+
+// Show Navbar After Jumbotron
+// =================
+
+(function($) {
+    $(document).ready(function() {
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function() {
+            $(window).scroll(function() {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 100) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+
+    });
+}(jQuery));
+
+
+// Hide Overlay Div & Play Youtube Video
+// =================
+
+$(document).ready(function() {
+  $('#box').on('click', function(ev) {
+
+    $("#box").css("display","none");
+    $("#video")[0].src += "&autoplay=1";
+    ev.preventDefault();
+
+  });
+});
