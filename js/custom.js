2295ce8641a2c441187f895a2daae06055a7e17e
Miles Dahmann <mdahmann@gmail.com>
2016-05-29 00:14:44 -0500

    partners carousel

--- js/custom.js
+++ js/custom.js
@@ -94,15 +94,14 @@ $('#partners-carousel').slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
-  variableWidth: true,
+  slidesToShow: 6,
   slidesToScroll: 4,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 6,
+        slidesToScroll: 4,
         infinite: true,
         dots: true
       }
@@ -110,15 +109,15 @@ $('#partners-carousel').slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 3,
+        slidesToScroll: 3
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 2,
+        slidesToScroll: 2
       }
     }
     // You can unslick at a given breakpoint now by adding:
