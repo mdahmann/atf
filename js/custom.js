06c4415996d4b9036419a1a76672ec8664ba0682
Miles Dahmann <mdahmann@gmail.com>
2016-05-29 00:22:52 -0500

    partner carousel

--- js/custom.js
+++ js/custom.js
@@ -100,8 +100,8 @@ $('#partners-carousel').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 6,
-        slidesToScroll: 4,
+        slidesToShow: 4,
+        slidesToScroll: 2,
         infinite: true,
         dots: true
       }
