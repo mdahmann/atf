77ecd04e906f2de64be95a8279aa0be3686b2e96
Miles Dahmann <mdahmann@gmail.com>
2016-05-29 00:06:16 -0500

    slick theme template

--- perch/templates/layouts/header.php
+++ perch/templates/layouts/header.php
@@ -18,6 +18,7 @@
     <link href="css/sass-space.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/slick.css" rel="stylesheet">
+    <link href="css/slick-theme.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
