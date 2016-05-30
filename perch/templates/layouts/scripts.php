61261bded8378d7edd3400aff1e99792c2de9d4b
Miles Dahmann <mdahmann@gmail.com>
2016-05-30 01:03:22 -0500

    jquery steps

--- perch/templates/layouts/scripts.php
+++ perch/templates/layouts/scripts.php
@@ -10,8 +10,9 @@
 </script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/slick.min.js"></script>
-<script src="js/multistep-modal.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/formvalidation/0.6.1/js/formValidation.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/formvalidation/0.6.1/js/framework/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/formvalidation/0.6.1/js/language/en_US.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-steps/1.1.0/jquery.steps.min.js"></script>
+
 <script src="js/custom.js"></script>
