aab9d438bd647fa49d6b928a47e43f1bc45e0b8b
Miles Dahmann <mdahmann@gmail.com>
2016-05-30 01:42:23 -0500

    better jquery anchor scroll

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,46 @@
 
 // Smooth Scrolling for Anchors Links
 // =================
-$(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top - 100
-                }, 750);
-                return false;
-            }
-        }
+/*******
+
+	***	Anchor Slider by Cedric Dugas ***
+  *** Version 2.0.0 ***
+	*** Http://www.position-absolute.com ***
+
+	Never have an anchor jumping your content, slide it.
+
+	Don't forget to put an id to your anchor !
+	You can use and modify this script for any project you want, but please leave this comment as credit.
+
+*****/
+
+(function ($) {
+  $.fn.anchorAnimate = function(options) {
+    var settings = $.extend({}, $.fn.anchorAnimate.defaults, options);
+
+    return this.each(function() {
+      var caller = this;
+      $(caller).click(function(event) {
+        event.preventDefault();
+        var elementClick = $(caller).attr("href");
+
+        var destination = $(elementClick).offset().top + settings.offset;
+        $("html:not(:animated), body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
+          if(0 === settings.offset) {
+            window.location.hash = elementClick;
+          }
+        });
+        return false;
+      });
     });
-});
+  };
+
+  $.fn.anchorAnimate.defaults = {
+    speed: 1100,
+    offset: 0
+  };
+}(jQuery));
+
 
 // Show Navbar After Jumbotron
 // =================
