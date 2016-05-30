05277a883c3dd599ce466d561e74c5f514856756
Miles Dahmann <mdahmann@gmail.com>
2016-05-29 19:56:26 -0500

    form modal

--- perch/templates/layouts/scripts.php
+++ perch/templates/layouts/scripts.php
@@ -10,4 +10,5 @@
 </script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/slick.min.js"></script>
+<script src="js/multistep-modal.js"></script>
 <script src="js/custom.js"></script>
