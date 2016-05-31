40ff5c09c6e6bc424d4d89b9eda257bd4492889e
Miles Dahmann <mdahmann@gmail.com>
2016-05-31 14:47:30 -0500

    added dev server

--- perch/config/config.php
+++ perch/config/config.php
@@ -8,12 +8,17 @@
     define("PERCH_DB_PREFIX", "perch2_");
     define("PERCH_IMAGE_LIB", 'imagick');
 
+      /* define("PERCH_DB_USERNAME", 'mdahmann_atf');
+    define("PERCH_DB_PASSWORD", 'GhostCa$h68');
+    define("PERCH_DB_SERVER", "localhost");
+    define("PERCH_DB_DATABASE", "mdahmann_atthefork");*/
+
     define('PERCH_TZ', 'UTC');
 
     define('PERCH_EMAIL_FROM', 'mdahmann@gmail.com');
     define('PERCH_EMAIL_FROM_NAME', 'Miles Dahmann');
 
-    define('PERCH_LOGINPATH', '/perch');
+    define('PERCH_LOGINPATH', '/at-the-fork/perch');
     define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
     define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
 
