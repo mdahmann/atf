d7f88b133a460c6afb22beb0bb865de55f50c86f
Miles Dahmann <mdahmann@gmail.com>
2016-05-31 00:05:28 -0500

    scroll offset

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 $('a[href*=#]').on('click', function(event) {
     event.preventDefault();
     $('html,body').animate({
-        scrollTop: $(this.hash).offset().top : 100;
+        scrollTop: $(this.hash).offset().top : 100
     }, 700);
 });
 
