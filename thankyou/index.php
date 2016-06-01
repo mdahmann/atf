926c2be719a31fe9b25a7f56ca71d9948e89a58e
Miles Dahmann <mdahmann@gmail.com>
2016-06-01 12:24:40 -0500

    adding thank you page

---
+++ thankyou/index.php
@@ -0,0 +1,34 @@
+<?php include ('perch/runtime.php'); ?>
+
+    <?php perch_layout('header'); ?>
+
+
+
+    <!-- Main jumbotron -->
+    <section class="jumbotron">
+        <div class="container-fluid">
+            <div class="col-xs-12 xl-mt-100 sm-mt-50 title text-center">
+              <?php perch_content ('Thank You Title'); ?>
+            </div>
+            <div class="col-xs-12 lg-mt-100 xs-mt-50 title text-center">
+              <?php perch_content ('Thank You Content'); ?>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- /Credits -->
+    <?php perch_content('Footer'); ?>
+
+    <!-- Trailer Modal -->
+    <?php perch_content('trailer-modal'); ?>
+
+    <?php perch_content('modal'); ?>
+
+    <?php perch_layout('scripts'); ?>
+
+</body>
+
+</html>
